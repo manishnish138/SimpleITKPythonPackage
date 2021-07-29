bf451d61b1fa1f279bea9ef566290beba4fd8d3b
manishnish138 <82148691+manishnish138@users.noreply.github.com>
2021-07-29 18:39:20 +0530

    Update dockcross-manylinux-build-wheels.sh

--- scripts/dockcross-manylinux-build-wheels.sh
+++ scripts/dockcross-manylinux-build-wheels.sh
@@ -3,12 +3,12 @@
 # Pull dockcross manylinux images
 #docker pull dockcross/manylinux-x64
 #docker pull dockcross/manylinux-x86
-docker pull dockcross/manylinux2014-aarch64
+#docker pull dockcross/manylinux2014-aarch64
 
 # Generate dockcross scripts
 #docker run dockcross/manylinux-x64 > /tmp/dockcross-manylinux-x64 && chmod u+x /tmp/dockcross-manylinux-x64
 #docker run dockcross/manylinux-x86 > /tmp/dockcross-manylinux-x86 && chmod u+x /tmp/dockcross-manylinux-x86
-docker run dockcross/manylinux2014-aarch64 > /tmp/dockcross-manylinux2014-aarch64 && chmod u+x /tmp/dockcross-manylinux2014-aarch64
+#docker run dockcross/manylinux2014-aarch64 > /tmp/dockcross-manylinux2014-aarch64 && chmod u+x /tmp/dockcross-manylinux2014-aarch64
 
 script_dir="`cd $(dirname $0); pwd`"
 
